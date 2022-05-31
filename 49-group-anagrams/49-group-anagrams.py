d55c11ca6463a9767545b295a85456f0b2db1ba6
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-05-31 19:33:10 -0400

    Time: 170 ms (30.43%), Space: 19.8 MB (25.67%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,10 +1,35 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    
+        ##### SOL 1 #####
+        anagrams = dict()
+        
+        for s in strs:
+            counts = [0] * 26
+            for c in s:
+                idx = ord(c) - ord('a')
+                counts[idx] += 1
+            
+            cur_list = anagrams.get(tuple(counts), [])
+            cur_list.append(s)
+            anagrams[tuple(counts)] = cur_list
+            
+        return anagrams.values()
+        
+        
+        ##### SOL 2 #####
+        # strs = [str_1 ... str_N]
+        # Runtime: O(n*m), w/ m = |str|*log(|str|)
+        # Memory: O(n)
+        
         result_dict = {} # {sorted str : [anagram_1 ... anagram_N] }
         for s in strs:
+            # get current anagram list
             sorted_s = ''.join(sorted(s))
             cur_list = result_dict.get(sorted_s, [])
+            
+            # add s to anagram list
             cur_list.append(s)
             result_dict[sorted_s] = cur_list
         
@@ -13,3 +38,4 @@ class Solution:
             result_list.append(v)
             
         return result_list
+        
