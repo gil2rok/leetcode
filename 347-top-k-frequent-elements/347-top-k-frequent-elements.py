da1b7a066301a2d42e2b5aef9c3f22052710cb04
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-07-28 17:29:54 -0400

    Time: 149 ms (59.79%), Space: 18.9 MB (34.13%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,26 +1,48 @@
 from collections import defaultdict
-from heapq import heappush, heapreplace, heappop
+from heapq import heappush, heapreplace, heappop, nsmallest
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return self.n_sol(nums, k)
+        return self.klogn_sol(nums, k)
         
     def n_sol(self, nums, k):
+        # count frequency with dict O(n)
         freq = dict()
         for i in nums:
             freq[i] = 1 + freq.get(i, 0)
         
+        # use bucket sort O(n)
         bucket = [[] for i in range(len(nums) + 1)]
         for num, count in freq.items():
             bucket[count].append(num)
             
+        # find k most frequent elements O(k)
         res = []
         for count in range(len(nums), 0, -1):
             for num in bucket[count]:
                 res.append(num)
                 if len(res) == k:
                     return res
+    
+    def klogn_sol(self, nums, k):
+        # count frequency with dict O(n)
+        freq = dict()
+        for n in nums:
+            freq[n] = 1 + freq.get(n, 0)
+            
+        # insert freq dict into min-heap O(n)
+        # negate frequency values b/c really want max-heap
+        heap = [(-c, n) for n, c in freq.items()]
+        heapify(heap)
         
+        # extract k most frequent nums
+        res = []
+        for _ in range(k):
+            (c, n) = heappop(heap)
+            res.append(n)
+        
+        return res
+            
     def nlogk_sol(self, nums, k):
         # initialize and fill frequency dict
         freq_dict = defaultdict(lambda: 0)
