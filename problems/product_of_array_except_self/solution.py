653edf27ff3c62a787331d1bbceb7e05b5ced4a0
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-08 03:19:39 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

---
+++ problems/product_of_array_except_self/solution.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * len(nums)
+        
+        # compute prefix
+        pre = 1
+        for i in range(len(nums)):
+            answer[i] = pre
+            pre *= nums[i]
+                        
+        # compute postfix
+        post = 1
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= post
+            post *= nums[i]
+            
+        return answer
+            
+        
