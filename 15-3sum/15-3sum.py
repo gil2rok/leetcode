69dc3e71ee60250fc1637187b5f4e87c93f4c915
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-15 16:32:01 -0400

    Time: 4157 ms (18.58%), Space: 17.2 MB (91.68%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: List[int]) -> List[List[int]]: 
         sol = set() # set of sorted tuples
         nums = sorted(nums) # O(nlogn)
         
@@ -7,22 +7,18 @@ class Solution:
             l, r = 0, len(nums) - 1
             
             # ensure l and r pointers are not at idx
-            #print('IDX:\t', idx, l)
             if l == idx: l += 1
             if r == idx: r -=1
             
-            #print(nums, -target)
-            while l < r: # O(n)
-                #print(nums[l], nums[r], nums[l]+nums[r]+target)
+            while l < r:
+                threesum = nums[l] + nums[r] + target
                 
-                # if sum of three nums == 0 without duplicates, add to sol
-                if nums[l] + nums[r] + target == 0:
-                    
-                    # check candidate sol is not already in solution set
-                    candidate_sol = tuple(sorted([nums[l], nums[r], target])) 
-                    if candidate_sol not in sol: #O(1)
-                        #print('Adding sol:\t', candidate_sol)
-                        sol.add(candidate_sol) #O(1)
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                elif threesum == 0:
+                    sol.add(tuple(sorted([nums[l], nums[r], target])) ) #O(1)
                         
                     # move l and r pointers until reach new, unique val
                     # to avoid adding redundant triples (nums[l], nums[r], target)
@@ -32,18 +28,7 @@ class Solution:
                     while nums[r] == r_temp and l<r:
                         r -= 1
                     
-                # if sum of three nums > 0, decrement right pointer
-                elif nums[l] + nums[r] + target > 0:
-                    r -=1
-                    
-                # if sum of three nums < 0, increment left pointer
-                elif nums[l] + nums[r] + target < 0:
-                    l += 1
-                    
                 # ensure l and r pointers are not at idx
-                #print('IDX:\t', idx, l)
                 if l == idx: l += 1
                 if r == idx: r -=1
-                    
-            #print('\n')
         return sol
