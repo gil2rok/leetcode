542544e29c3926f6b3f8fd9eefb416fa5f145b1a
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-17 11:07:15 -0400

    Time: 2733 ms (25.70%), Space: 17.4 MB (88.78%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,34 +1,62 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]: 
-        sol = set() # set of sorted tuples
-        nums = sorted(nums) # O(nlogn)
+        sol = set()
+        nums.sort() # O(nlogn)
         
-        for idx, target in enumerate(nums): #O(n)
-            l, r = 0, len(nums) - 1
+        for idx, target in enumerate(nums): # O(n)
+            l, r = idx + 1, len(nums) - 1
             
-            # ensure l and r pointers are not at idx
-            if l == idx: l += 1
-            if r == idx: r -=1
+            #print(nums, target)
             
-            while l < r:
+            while l < r: # O(n)
                 threesum = nums[l] + nums[r] + target
+                
+                #print(nums[l], nums[r], threesum)
                 
                 if threesum > 0:
                     r -= 1
                 elif threesum < 0:
                     l += 1
                 elif threesum == 0:
-                    sol.add(tuple(sorted([nums[l], nums[r], target])) ) #O(1)
-                        
-                    # move l and r pointers until reach new, unique val
-                    # to avoid adding redundant triples (nums[l], nums[r], target)
-                    l_temp, r_temp = nums[l], nums[r]
-                    while nums[l] == l_temp and l<r:
-                        l += 1
-                    while nums[r] == r_temp and l<r:
-                        r -= 1
+                    sol.add(tuple([nums[l], nums[r], target]))
                     
-                # ensure l and r pointers are not at idx
-                if l == idx: l += 1
-                if r == idx: r -=1
+                    while l<r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l<r and nums[r] == nums[r-1]:
+                        r -= 1     
+                    l += 1
         return sol
+        
+        
+#         sol = set() # set of sorted tuples
+#         nums.sort() # O(nlogn)
+        
+#         for idx, target in enumerate(nums): #O(n)
+#             l, r = 0, len(nums) - 1
+            
+#             # ensure l and r pointers are not at idx
+#             if l == idx: l += 1
+#             if r == idx: r -=1
+            
+#             while l < r:
+#                 threesum = nums[l] + nums[r] + target
+                
+#                 if threesum > 0:
+#                     r -= 1
+#                 elif threesum < 0:
+#                     l += 1
+#                 elif threesum == 0:
+#                     sol.add(tuple(sorted([nums[l], nums[r], target])) ) #O(1)
+                        
+#                     # move l and r pointers until reach new, unique val
+#                     # to avoid adding redundant triples (nums[l], nums[r], target)
+#                     l_temp, r_temp = nums[l], nums[r]
+#                     while nums[l] == l_temp and l<r:
+#                         l += 1
+#                     while nums[r] == r_temp and l<r:
+#                         r -= 1
+                    
+#                 # ensure l and r pointers are not at idx
+#                 if l == idx: l += 1
+#                 if r == idx: r -=1
+#         return sol
