98df656745633bb40cf9190d91bc665bb87be12e
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-17 10:20:16 -0400

    Time: 1434 ms (16.56%), Space: 26.9 MB (93.56%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         largest_area = 0
-        
         l, r = 0, len(height) - 1
+        
         while l < r:
             cur_area = self.compute_area(l, r, height)
             largest_area = max(largest_area, cur_area)
@@ -16,75 +16,4 @@ class Solution:
     def compute_area(self, i, j , height):
         assert(i < j)
         return (j-i) * min(height[i], height[j])
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         l = 0; r = len(height) - 1
-#         max_area = 0
-        
-#         while l < r: # while pointers haven't met yet
-#             area = self.compute_area(height, l, r)
-#             max_area = max(area, max_area)
-            
-#             if height[l] > height[r]:
-#                 r-= 1
-#             else:
-#                 l += 1
-#         return max_area
-        
-#     def compute_area(self, height, l, r):
-#         h = min(height[l], height[r])
-#         w = r - l
-#         return h * w
-    
     
