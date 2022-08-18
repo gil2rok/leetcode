0dfec4c4b0c4d169e42878780992350fe90d6c63
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-18 14:36:50 -0400

    Time: 1800 ms (34.03%), Space: 24.8 MB (95.11%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -3,9 +3,10 @@ class Solution:
         max_profit = 0
         l, r = 0, 1 # left = buy, right = sale
         
-        for r in range(len(prices)):
+        while r < len(prices):
             if prices[l] < prices[r]: # if profitable
                 max_profit = max(max_profit, prices[r] - prices[l])
             else:
-                l = r    
+                l = r
+            r += 1     
         return max_profit
