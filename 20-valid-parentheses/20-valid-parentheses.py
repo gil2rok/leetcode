e8b2e11de37ae891d603bd7b64d230e6d10abc32
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-18 22:28:49 -0400

    Time: 47 ms (56.74%), Space: 13.9 MB (71.96%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,19 +1,34 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        map = {')' : '(', ']' : '[', '}' : '{'}
         stack = []
         
         for c in s:
-            # add open paren to stack
-            if c == '(' or c=='[' or c == '{':
-                stack.append(self.opposite(c))
+            if c not in map:
+                stack.append(c)
+                continue
+            if not stack or (map[c] != stack[-1]):
+                return False
+            stack.pop()
+            
+        return not stack
+            
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+        
+        
+#         for c in s:
+#             # add open paren to stack
+#             if c == '(' or c=='[' or c == '{':
+#                 stack.append(self.opposite(c))
                 
-            # if have close paren, check that it is on top of stack
-            # (and also check that stack is not empty) 
-            else:
-                if len(stack) == 0 or c != stack[-1]:
-                    return False
-                stack.pop()
-        return len(stack) == 0
+#             # if have close paren, check that it is on top of stack
+#             # (and also check that stack is not empty) 
+#             else:
+#                 if len(stack) == 0 or c != stack[-1]:
+#                     return False
+#                 stack.pop()
+#         return len(stack) == 0
     
     def opposite(self, c):
         if c == '(':
