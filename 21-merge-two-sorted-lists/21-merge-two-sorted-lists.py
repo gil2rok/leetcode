c441272ae59601de39c1abfa937dc398baf780be
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-26 13:44:35 -0400

    Time: 60 ms (47.35%), Space: 13.9 MB (32.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,43 +5,62 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # edge case if list1 or list2 is empty
-        if list1 == None:
-            return list2
-        if list2 == None:
-            return list1
+        dummy = ListNode()
+        tail = dummy
         
-        # init head and pointer
-        if list1.val > list2.val:
-            head, pt = list2, list2
-            list2 = list2.next
-        else:
-            head, pt = list1, list1
-            list1 = list1.next
-            
-        n1, n2 = list1, list2
-        
-        # while not reached end of list1 AND list2
-        while n1 != None and n2 != None:
-            if n1.val >= n2.val:
-                pt.next = n2
-                pt = pt.next
-                n2 = n2.next
+        while list1 != None and list2 != None:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
             else:
-                pt.next = n1
-                pt = pt.next
-                n1 = n1.next
-            
-        # add all remaining elements of list1
-        while n1 != None:
-            pt.next = n1
-            pt = pt.next
-            n1 = n1.next
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
         
-        # add all remaining elements of list2
-        while n2 != None:
-            pt.next = n2
-            pt = pt.next
-            n2 = n2.next
-        return head
+        if list1 != None:
+            tail.next = list1
+        if list2 != None:
+            tail.next = list2
+        
+        return dummy.next
+        
+#         # edge case if list1 or list2 is empty
+#         if list1 == None:
+#             return list2
+#         if list2 == None:
+#             return list1
+        
+#         # init head and pointer
+#         if list1.val > list2.val:
+#             head, pt = list2, list2
+#             list2 = list2.next
+#         else:
+#             head, pt = list1, list1
+#             list1 = list1.next
+            
+#         n1, n2 = list1, list2
+        
+#         # while not reached end of list1 AND list2
+#         while n1 != None and n2 != None:
+#             if n1.val >= n2.val:
+#                 pt.next = n2
+#                 pt = pt.next
+#                 n2 = n2.next
+#             else:
+#                 pt.next = n1
+#                 pt = pt.next
+#                 n1 = n1.next
+            
+#         # add all remaining elements of list1
+#         while n1 != None:
+#             pt.next = n1
+#             pt = pt.next
+#             n1 = n1.next
+        
+#         # add all remaining elements of list2
+#         while n2 != None:
+#             pt.next = n2
+#             pt = pt.next
+#             n2 = n2.next
+#         return head
         
