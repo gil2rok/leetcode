08c45e16c2400639456cef3719ef9acca711a92f
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-30 14:55:36 -0400

    Time: 481 ms (9.46%), Space: 13.9 MB (57.56%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -4,19 +4,20 @@ class Solution:
         max_len = 0 # len of max substr that contains repeating letter after k transformations
         
         l = 0
-        majority_count = 0 # num times majority char appears in cur substr
+        #majority_count = 0 # num times majority char appears in cur substr
         for r in range(len(s)): #O(n)
             counts[s[r]] += 1
-            majority_count = max(majority_count, counts[s[r]])
+            #majority_count = max(majority_count, counts[s[r]])
 
             # while cannot transform cur str into a str containing only 1 letter using k transformations
-            while majority_count + k < len(s[l:r+1]):
+            #while majority_count + k < len(s[l:r+1]):
+            while max(counts.values()) + k < len(s[l:r+1]):
                 counts[s[l]] -= 1 # decrement count dict
-                majority_count -= 1
+                #majority_count -= 1
                 
                 # find next majority count in O(26)
-                for v in counts.values():
-                    majority_count = max(majority_count, v)
+                #for v in counts.values():
+                #    majority_count = max(majority_count, v)
                
                 l += 1
             max_len = max(max_len, len(s[l:r+1]))
