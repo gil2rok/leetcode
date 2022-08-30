f5a73d94cbff458eba3cef90772f2c7e5640f823
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-30 16:47:22 -0400

    Time: 67 ms (44.48%), Space: 14.1 MB (63.46%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -9,16 +9,15 @@ class Solution:
                 return nums[l]
             
             mid = (l+r) // 2
-            #print(nums[l:r+1], '\t', nums[mid], l, r)
             
-            # if left subarray properly sorted
+            # if left subarray sorted
             if nums[l] <= nums[mid]:
                 if nums[l] < nums[r]:
                     return nums[l]
                 else:
                     l = mid + 1
                     
-            # if right subarray properly sorted
+            # if right subarray sorted
             else:
                 if nums[mid] < nums[mid-1]:
                     return nums[mid]
