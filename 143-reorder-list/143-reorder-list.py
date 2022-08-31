92067bad3b87f106e1ce6c1ab05d42cb25fd6bbc
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-31 10:20:57 -0400

    Time: 100 ms (86.25%), Space: 24 MB (64.19%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -9,94 +9,58 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         
-        loc = dict() # idx in linked list : node
-        idx = 0
-        pt = head
+        # find middle
+        slow, fast = head, head
+        while fast != None and fast.next != None and fast.next.next != None:
+            slow = slow.next
+            fast = fast.next.next
         
-        # add all elements to loc dict
-        while pt != None:
-            loc[idx] = pt
-            pt = pt.next
-            idx += 1
+        # separate into two lists
+        second = slow.next
+        slow.next = None
         
-        # while l, r pointers have not met 
-        l, r, = 0, idx-1 # considering even and odd length linked lists
-        while l + 1 < r:
+        # reverse second list
+        prev = None
+        while second != None:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        # merge lists
+        first, second = head, prev
+        while first != None and second != None:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
             
-            # redirect pointers in list
-            loc[r-1].next = None
-            loc[r].next = loc[l+1]
-            loc[l].next = loc[r]
-            
-            # increment l, r pointers
-            l += 1
-            r -= 1
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            first = tmp1
+            second = tmp2
         
         
         
-#         # edge case if linked list is empty, contains 1 element, or contains 2 elements
-#         # TODO: do I want to check case when head.next.next == None?
-#         if head == None or head.next == None or head.next.next == None:
-#             return head
-    
-#         prev = head
-#         cur = head.next
+       
         
-#         # construct doubely linked-list
-#         dll = [] # doublely linked-list
-#         dll.append((None, cur))
-#         while cur != None:
-#             print(dll)
-#             dll.append((prev, cur.next))
-#             prev = cur
-#             cur = cur.next
-            
-#         for i in range(len(dll) // 2):
-#             dll[-i]
-            
-            
-#         Naive Approach O(n^2)
+#         ##### Time O(n) | Space O(n) #####
+#         loc = dict() # idx in linked list : node
+#         idx = 0
 #         pt = head
-#         while pt.next.next != None:
-#             tail2 = pt
+        
+#         # add all elements to loc dict
+#         while pt != None:
+#             loc[idx] = pt
+#             pt = pt.next
+#             idx += 1
+        
+#         # while l, r pointers have not met 
+#         l, r, = 0, idx-1 # considering even and odd length linked lists
+#         while l + 1 < r:
             
-#             while tail2.next.next != None:
-#                 tail2 = tail2.next
-#             tail = tail2.next
-#             tail2.next = None
-                    
-#             tmp = pt.next
-#             pt.next = tail
-#             tail.next = tmp
-#             pt = tmp
-#         return head
+#             # redirect pointers in list
+#             loc[r-1].next = None
+#             loc[r].next = loc[l+1]
+#             loc[l].next = loc[r]
+            
+#             # increment l, r pointers
+#             l += 1
+#             r -= 1
