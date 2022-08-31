3cb781dabf57e00a04113de14d627e88c822259c
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-31 12:29:22 -0400

    Time: 63 ms (34.59%), Space: 14.9 MB (83.42%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+from heapq import heappush, heappop
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        copy_head = Node(0)
+        cur, copy = head, copy_head
+        orig_dict = dict() # original node --> copy node
+        
+        # copy .next attribute
+        while cur != None:
+            copy.next = Node(cur.val)
+            copy = copy.next
+            orig_dict[cur] = copy
+            cur = cur.next
+                    
+        # copy .random attribute
+        cur, copy = head, copy_head.next
+        while cur != None:
+            if cur.random == None:
+                copy.random = None
+            else:
+                copy.random = orig_dict[cur.random]
+            cur = cur.next
+            copy = copy.next
+            
+        return copy_head.next
+        
