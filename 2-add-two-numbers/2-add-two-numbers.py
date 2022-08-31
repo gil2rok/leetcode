a8ba22ba5f65cf5ae26f2d616db5d7c1d0118c15
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-31 13:05:25 -0400

    Time: 129 ms (23.88%), Space: 14.1 MB (10.10%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -6,46 +6,122 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l3 = ListNode()
+        p1, p2 = l1, l2
         carry = 0
-        
-        p1 = l1
-        p2 = l2
-        p3 = l3
-        
-        while p1.next != None and p2.next != None:
-            p3.val = (p1.val + p2.val + carry) % 10
-            carry = (p1.val + p2.val + carry) >= 10
+        res = ListNode()
+        dummy = res
+        while p1 != None and p2 != None:
+            digit = (p1.val + p2.val + carry) % 10
+            dummy.next = ListNode(val=digit)
             
-            p3.next = ListNode()
-            
+            # increment pointers and update carry
+            carry = (p1.val + p2.val + carry) // 10
+            dummy = dummy.next
             p1 = p1.next
             p2 = p2.next
-            p3 = p3.next
-        
-        p3.val = (p1.val + p2.val + carry) % 10
-        carry = (p1.val + p2.val + carry) >= 10
-        
-        while p1.next != None:
-            p3.next = ListNode()
-            p3 = p3.next
+            
+        while p1 != None:
+            digit = (p1.val + carry) % 10
+            dummy.next = ListNode(val=digit)
+            
+            carry = (p1.val + carry) // 10
+            dummy = dummy.next
             p1 = p1.next
-            
-            p3.val = (p1.val + carry) % 10
-            carry = (p1.val + carry) >= 10
-           
-        while p2.next != None:
-            p3.next = ListNode()
-            p3 = p3.next
-            p2 = p2.next
-            
-            p3.val = (p2.val + carry) % 10
-            carry = (p2.val + carry) >= 10
 
-        if carry == 1:
-            p3.next = ListNode()
-            p3 = p3.next
-            p3.val = 1
+        while p2 != None:
+            digit = (p2.val + carry) % 10
+            dummy.next = ListNode(val=digit)
             
-        return l3
+            carry = (p2.val + carry) // 10
+            dummy = dummy.next
+            p2 = p2.next
+            
+        if carry == 1: dummy.next = ListNode(carry)
+
+        return res.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         l3 = ListNode()
+#         carry = 0
+        
+#         p1 = l1
+#         p2 = l2
+#         p3 = l3
+        
+#         while p1.next != None and p2.next != None:
+#             p3.val = (p1.val + p2.val + carry) % 10
+#             carry = (p1.val + p2.val + carry) >= 10
+            
+#             p3.next = ListNode()
+            
+#             p1 = p1.next
+#             p2 = p2.next
+#             p3 = p3.next
+        
+#         p3.val = (p1.val + p2.val + carry) % 10
+#         carry = (p1.val + p2.val + carry) >= 10
+        
+#         while p1.next != None:
+#             p3.next = ListNode()
+#             p3 = p3.next
+#             p1 = p1.next
+            
+#             p3.val = (p1.val + carry) % 10
+#             carry = (p1.val + carry) >= 10
+           
+#         while p2.next != None:
+#             p3.next = ListNode()
+#             p3 = p3.next
+#             p2 = p2.next
+            
+#             p3.val = (p2.val + carry) % 10
+#             carry = (p2.val + carry) >= 10
+
+#         if carry == 1:
+#             p3.next = ListNode()
+#             p3 = p3.next
+#             p3.val = 1
+            
+#         return l3
  
