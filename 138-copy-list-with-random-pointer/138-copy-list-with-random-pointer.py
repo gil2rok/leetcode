59c516c3a148d1e126439b16af80dcda2c712ca2
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-08-31 12:42:15 -0400

    Time: 73 ms (14.96%), Space: 14.8 MB (83.42%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -9,28 +9,49 @@ class Node:
 from heapq import heappush, heappop
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        ##### Time O(2n) | Space O(n) #####
-        copy_head = Node(0) # dummy node
-        orig_dict = dict() # original node --> copy node
+    ##### Time O(2n) | Space O(n) #####
+    # space excludes the deep copy itself
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]': 
+        oldToCopy = {None : None} # old node --> deep copy of old node
         
-        # copy .next attribute
-        cur, copy = head, copy_head
+        # fill dict with old and copy nodes
+        cur = head
         while cur != None:
-            copy.next = Node(cur.val)
-            copy = copy.next
-            orig_dict[cur] = copy
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
             cur = cur.next
-                    
-        # copy .random attribute
-        cur, copy = head, copy_head.next
+        
+        cur = head
         while cur != None:
-            if cur.random == None:
-                copy.random = None
-            else:
-                copy.random = orig_dict[cur.random]
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
             cur = cur.next
-            copy = copy.next
+        return oldToCopy[head]
             
-        return copy_head.next
         
+#     ##### Time O(2n) | Space O(n) #####
+#     # space excludes the deep copy itself
+#     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]': 
+#         copy_head = Node(0) # dummy node
+#         orig_dict = dict() # original node --> copy node
+        
+#         # copy .next attribute
+#         cur, copy = head, copy_head
+#         while cur != None:
+#             copy.next = Node(cur.val)
+#             copy = copy.next
+#             orig_dict[cur] = copy
+#             cur = cur.next
+                    
+#         # copy .random attribute
+#         cur, copy = head, copy_head.next
+#         while cur != None:
+#             if cur.random == None:
+#                 copy.random = None
+#             else:
+#                 copy.random = orig_dict[cur.random]
+#             cur = cur.next
+#             copy = copy.next
+            
+#         return copy_head.next
