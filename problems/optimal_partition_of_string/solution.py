b408c39961a98b98164decb734085f1453e42188
gil2rok <36947659+gil2rok@users.noreply.github.com>
2022-09-14 22:24:03 +0000

    Sync LeetCode submission - Optimal Partition of String (python3)

--- problems/optimal_partition_of_string/solution.py
+++ problems/optimal_partition_of_string/solution.py
@@ -4,19 +4,16 @@ class Solution:
         cur = dict()
         
         l, r = 0, 0
-        while r < len(s):
+        for r in range(len(s)):
             new_char = s[r]
             
             if new_char in cur:  # make new substr
-                #print(cur)
                 num_partitions += 1
                 l = r
                 cur = {new_char : 1}
             else:  # add new_char to cur substr
                 cur[new_char] = 1
-            r += 1
             
-        #print(cur)
         return num_partitions + (len(cur) != 0)
             
             
