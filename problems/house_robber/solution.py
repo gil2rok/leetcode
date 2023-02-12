57a8796fd5db3b6f6218274fc3df3cfef0b5b642
gil2rok <36947659+gil2rok@users.noreply.github.com>
2023-02-12 18:12:51 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,23 +1,13 @@
 class Solution:
-    ##### Time O(n) | Space O(n) #####
     def rob(self, nums: List[int]) -> int:
-        # edge cases
+        # edge case
         if len(nums) == 1:
             return nums[0]
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-        
-        # init dp array
-        dp = [-1] * len(nums)
-        
-        # setup intial dp values
-        dp[len(nums)-1] = nums[-1]
-        dp[len(nums) - 2] = max(nums[-1], nums[-2])
-        
-        # iterate through dp recurrence
-        for i in range(len(nums)-3, -1, -1): 
-            dp[i] = max(nums[i] + dp[i+2], dp[i+1])
-            
-        return dp[0]
-            
-        
+
+        t2, t1 = nums[0], max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            tmp = t1
+            t1 = max(nums[i] + t2, t1)
+            t2 = tmp
+
+        return t1
