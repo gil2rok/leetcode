8d2a545b80bf1d17ded36717d2ec7cacb3022cbf
gil2rok <36947659+gil2rok@users.noreply.github.com>
2023-05-31 03:27:01 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (python3)

--- problems/kth_smallest_element_in_a_bst/solution.py
+++ problems/kth_smallest_element_in_a_bst/solution.py
@@ -7,28 +7,21 @@
 
 import math
 from heapq import heappush, heappop, heappushpop
+from collections import deque
 
 class Solution:
-    # Time: O(n*log(k)) | Space: O(k)
+    # Time: O(k) | Space: O(k)
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        smallest, counter = [], 0
-        largest_k = root.val
+        # in-order traversal via DFS and implemented with recursion
+        stack = []
+        cur = root
 
-        q = collections.deque([root])
-        while q: # while have not traversed entire BST
-            node = q.popleft() # extract current node
-            if not node: continue # skip null node
-            
-            if counter < k: # when do NOT have k smallest elements
-                heappush(smallest, -node.val) # O(log(k))
-                largest_k = max(largest_k, node.val)
-                counter += 1
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
 
-            else: # when do have k smallest elements
-                if node.val < largest_k: # if found new smaller element
-                    heappushpop(smallest, -node.val) # O(log(k))
-                    largest_k = smallest[0] * -1
-
-            q.append(node.left)
-            q.append(node.right)
-        return largest_k
+            cur = stack.pop()
+            k -= 1
+            if k == 0: return cur.val
+            cur = cur.right
