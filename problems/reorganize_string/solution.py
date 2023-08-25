f039488d1da660f5680e9ee9c778ab519efed8a6
gil2rok <36947659+gil2rok@users.noreply.github.com>
2023-08-25 15:30:56 +0000

    Sync LeetCode submission - Reorganize String (python3)

--- problems/reorganize_string/solution.py
+++ problems/reorganize_string/solution.py
@@ -1,54 +1,26 @@
 import heapq
+from collections import Counter
 class Solution:
+    """
+    n = len(s)  k = len(set(s))
+    Time:   O( nlog(k) )
+    Space:  O( n )
+    """
     def reorganizeString(self, s: str) -> str:
-        # init heap and res
-        heap = [(-count, time, char)for time, (char, count) in enumerate(Counter(s).items())]
+        heap = [(-v, k) for k, v in Counter(s).items()]
         heapq.heapify(heap)
 
-        self.time = len(s)
-        res = ""
-        
-        # add most freq char to res
-        print(res, "\t", heap)
+        prev_a, prev_b = heapq.heappop(heap)
+        res = prev_b
+        prev_a += 1
 
-        count, char = self.my_pop(heap)
-        res += char
-        count -= 1
-        self.my_push(heap, count, char)
-        self.time += 1
+        while heap:
+            cur_a, cur_b = heapq.heappop(heap)
+            res += cur_b
+            cur_a += 1
 
-        for _ in range(len(s) - 1):
-            print(res, "\t", heap)
-            count, char = self.my_pop(heap) 
+            if prev_a < 0:
+                heapq.heappush(heap, (prev_a, prev_b)) # O(logk)
+            prev_a, prev_b = cur_a, cur_b
 
-            # if most freq char was just added to res
-            if char == res[-1]:
-                if (len(heap) == 0) or (len(heap) == 1):
-                    return ""
-
-                count2, char2 = self.my_pop(heap)
-                res += char2
-                count2 -= 1
-                self.my_push(heap, count2, char2)
-                self.time += 1
-
-                self.my_push(heap, count, char)
-                self.time += 1
-            else:
-                res += char
-                count -= 1
-                self.my_push(heap, count, char)
-                self.time += 1
-
-        print(res, "\t", heap)
-        return res
-            
-    def my_push(self, heap, count, char):
-        if count > 0:
-            neg_count = -count
-            heapq.heappush(heap, (neg_count, self.time, char))
-
-    def my_pop(self, heap):
-        neg_count, _, char = heapq.heappop(heap)
-        count = -neg_count
-        return count, char
+        return res if prev_a == 0 else ""
